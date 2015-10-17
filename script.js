7100d1aa9cc0cdf28b8b2726a4e08610307d8333
Hemant-Mann <hemant.mann121@gmail.com>
2015-10-17 10:51:28 +0530

    Added Event handlers

--- script.js
+++ script.js
@@ -5,4 +5,11 @@
 
 	map = mapster.create(element, options);
 	map.zoom(13);
+
+	map._on('click', function (e) {
+		// do something with the event
+		console.log(e);
+	});
+	// similarly we can add more events
+	
 }(window, window.Mapster || (window.Mapster = {})));
